4a3f5a3cbad75f3e92328e9c71b81c209b82608a
Igor dos Reis <igorreis@gmail.com>
2022-06-11 16:54:14 -0300

    Exercicio 3 da parte 2 do bloco 7.1.

--- 1-fundamentos/b07-js-ES6-testes-unitarios/d01-let-const-arrow-functions-template-literals/exercicio5.js
+++ 1-fundamentos/b07-js-ES6-testes-unitarios/d01-let-const-arrow-functions-template-literals/exercicio5.js
@@ -1,11 +1,17 @@
-const clicksDisplay = document.querySelector('p');
-const button = document.querySelector('button');
+// const clicksDisplay = document.querySelector('p');
+// const button = document.querySelector('button');
 
-const clickCounter = () => {
-    counter = 0;
-    button.addEventListener('click', () => {
-        counter += 1;
-        clicksDisplay.innerText = counter;
-    });
-}
-clickCounter();
+// const clickCounter = () => {
+//     let counter = 0;
+//     button.addEventListener('click', () => {
+//         counter += 1;
+//         clicksDisplay.innerText = counter;
+//     });
+// }
+// clickCounter();
+
+let counter = 0;
+document.querySelector('button').addEventListener('click', () => {
+    counter += 1;
+    document.querySelector('p').innerText = counter;
+})
