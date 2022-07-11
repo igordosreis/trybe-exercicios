ac856a164c4f03505073725b385a0a6c924d9162
Igor dos Reis <igorreis@gmail.com>
2022-07-11 18:01:58 -0300

    Exercicios do bloco 9.2.

---
+++ 2-front-end/b09-js-testes-assincronos/d02-fetch-api-async-await/exercicio2.js
@@ -0,0 +1,58 @@
+// // Com essa sintaxe, o objeto com os dados da API só é acessível com o data.data do segundo .then.
+// // Mesmo se fosse criada uma variavel para receber o fetch, ela só seria uma Promise.
+// const fetchCoins = () => {  
+//     fetch('https://api.coincap.io/v2/assets')
+//       .then((response) => response.json())
+//       .then((data) => data.data)
+//       .catch((error) => error.toString());
+// }
+// fetchCoins();
+
+// // Com essa sintaxe, os dados da API sao acessiveis pela variavel data.
+const fetchCoins = async () => {
+    try { 
+        const response = await fetch('https://api.coincap.io/v2/assets');
+        const data = await response.json();
+        return data.data;
+    } catch {
+        error.toString();
+    }
+}
+// fetchCoins();
+
+// // Sintaxe 'hibrida' do gabarito. Os dados da API sao acessiveis pela variavel coins.
+// const fetchCoins = async () => {
+//   const url = 'https://api.coincap.io/v2/assets';
+//   const coins = await fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data.data)
+//     .catch((error) => error.toString());
+//   return coins;
+// }
+// fetchCoins();
+
+const getUsdConversor = async () => {
+    const url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json';
+    const brlPriceList = await fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.usd)
+        .catch((error) => error);
+    return brlPriceList;
+}
+
+// // Para trabalhar com o retorno de fetchCoins, essa funcao tambem precisa ser assincrona.
+const createTop10List = async () => { 
+    const getCoins = await fetchCoins(); // A sintaxe fetchCoins().slice(0, 10) não funciona
+    const brlPrice = await getUsdConversor();
+    const ul = document.querySelector('ul');
+    getCoins
+        .slice(0, 10)
+        // .filter((coin, index) => index < 10)
+        .forEach((coin) => {
+            const li = document.createElement('li');
+            const usdPrice = Number(coin.priceUsd);
+            li.innerText = `${coin.name} (${coin.symbol}): USD ${usdPrice.toFixed(3)} | BRL: ${(usdPrice * brlPrice.brl).toFixed(3)}`
+            ul.appendChild(li);
+        });
+}
+createTop10List();
