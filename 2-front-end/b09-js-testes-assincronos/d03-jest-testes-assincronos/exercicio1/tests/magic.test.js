88d3a784d2e399b024a53001e966a72dc217a71f
Igor dos Reis <igorreis@gmail.com>
2022-07-12 15:30:19 -0300

    Adicionando expect.assertions(1)

--- 2-front-end/b09-js-testes-assincronos/d03-jest-testes-assincronos/exercicio1/tests/magic.test.js
+++ 2-front-end/b09-js-testes-assincronos/d03-jest-testes-assincronos/exercicio1/tests/magic.test.js
@@ -4,30 +4,36 @@ const { card } = require('../simulator/card');
 
 describe('Testa a função getMagicCard', () => {
   it('Deve possuir a propriedade name com o valor Ancestor\'s Chosen', async () => {
+    expect.assertions(1);
     const response = await getMagicCard('130550');
     const expected = 'Ancestor\'s Chosen';
     expect(response.name).toEqual(expected);
   });
   it('Verifica se getMagicCard é uma função', async () => {
+    expect.assertions(1);
     const result = (typeof getMagicCard);
     const expected = 'function';
     expect(result).toEqual(expected);
   });
   it('Verifica se, ao chamar a função getMagicCard com o argumento "130550", a função fetch foi chamada', async () => {
+    expect.assertions(1);
     await getMagicCard('130550');
     expect(fetch).toHaveBeenCalled();
   });
   it('Verifica se, ao chamar a função getMagicCard com o argumento "130550", a função fetch foi chamada com o endpoint "https://api.magicthegathering.io/v1/cards/130550"', async () => {
+    expect.assertions(1);
     const url = 'https://api.magicthegathering.io/v1/cards/130550';
     await getMagicCard('130550');
     expect(fetch).toHaveBeenCalledWith(url);
   });
   it('Verifica se o retorno da função getMagicCard é igual ao objeto card', async () => {
+    expect.assertions(1);
     const response = await getMagicCard('130550');
     const expected = card;
     expect(response).toEqual(expected);
   });
   it('Verifica se, ao chamar a função getMagicCard com o argumento \'idDesconhecido\', retorna um erro com a mensagem: "Id is not found!"', async () => {
+    expect.assertions(1);
     const response = await getMagicCard('idDesconhecido');
     const expected = 'Id is not found!';
     expect(response).toEqual(new Error(expected));
