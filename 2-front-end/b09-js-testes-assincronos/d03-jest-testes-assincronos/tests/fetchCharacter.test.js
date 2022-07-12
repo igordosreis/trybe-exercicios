877860c0db8f89d0785cd6fd730b4a9cfc09560d
Igor dos Reis <igorreis@gmail.com>
2022-07-12 15:21:01 -0300

    Exercicios do bloco 9.3.

--- 2-front-end/b09-js-testes-assincronos/d03-jest-testes-assincronos/tests/fetchCharacter.test.js
+++ 2-front-end/b09-js-testes-assincronos/d03-jest-testes-assincronos/tests/fetchCharacter.test.js
@@ -5,10 +5,24 @@ describe('Teste a função fetchCharacter', () => {
   it('Verifica se o nome da personagem é Wonder Woman', async () => {
     const character = await fetchCharacter('720');
     expect(character.name).toEqual('Wonder Woman');
-    console.log(character)
+    // await expect(fetchCharacter('720')).resolves.toEqual({OBJETO RETORNADO});
+    // A sintaxe com o await na mesma linha e logo antes do expect requer o uso do .resolves ou 
+    // .rejects no caso de sucesso ou falha, respectivamente
   });
   it('Verifica se retorna erro ao executar a funcao sem parametro', async () => {
     const failRequest = await fetchCharacter();
     expect(failRequest).toEqual(new Error('You must provide an url'));
+    // await expect(fetchCharacter()).resolves.toEqual(new Error('You must provide an url'));
+  });
+  it('Verifica se retorna \'Invalid id\' ao executar a funcao com parametro que nao existe', async () => {
+    const response = await fetchCharacter('string');
+    expect(response).toEqual('Invalid id');
+    // await expect(fetchCharacter('string')).resolves.toEqual('Invalid id');
+  });
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
   });
 });
