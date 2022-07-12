bbc850300206b1e71eb909a3902d0658a894fe52
Igor dos Reis <igorreis@gmail.com>
2022-07-12 16:09:15 -0300

    Exercicio bonus do dia 9.3.

--- 2-front-end/b09-js-testes-assincronos/d03-jest-testes-assincronos/exercicio2/tests/magic.test.js
+++ 2-front-end/b09-js-testes-assincronos/d03-jest-testes-assincronos/exercicio2/tests/magic.test.js
@@ -2,21 +2,23 @@ const { saveFavoriteMagicCard } = require('../src/magic.js');
 const favoriteCards = require('../data/favoriteCards.js');
 
 const retrievesFavoriteCards = () => {
-  // implemente sua função aqui
+  favoriteCards.splice(4, favoriteCards.length - 4);
 }
 
 describe(' Testa a função saveFavoriteMagicCard', () => {
+  afterEach(() => retrievesFavoriteCards());
   it('Testa se um novo card é adicionado a cada execução', async () => {
-    expect.assertions();
+    expect.assertions(3);
     await saveFavoriteMagicCard('130553');
-
-    // implemente seus testes aqui
+    expect(favoriteCards.length).toEqual(5);
+    expect(favoriteCards[4].name).toEqual('Beacon of Immortality');
+    await saveFavoriteMagicCard('130554');
+    expect(favoriteCards.length).toEqual(6);
   });
-
   it('Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
-    // expect.assertions();
-    // expect(favoriteCards).toHaveLength(4);
-
-    // implemente seus testes aqui
+    expect.assertions(2);
+    expect(favoriteCards).toHaveLength(4);
+    const cardNames = favoriteCards.map((card) => card.name);
+    expect(cardNames).toEqual(['Ancestor\'s Chosen', 'Angel of Mercy', 'Aven Cloudchaser', 'Ballista Squad']);
   });
 });
