42dcc2f40c43232ea580d6b5b33956d9dac01830
Igor dos Reis <igorreis@gmail.com>
2022-07-15 22:00:47 -0300

    Exercicios do bloco 10.1.

---
+++ 2-front-end/b10-intro-react/d01-intro-react/exercicio-2/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+        <Footer/>
+      </div>
+    )
+  }
+}
+
+export default App;
