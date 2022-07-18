d46d5d1ab34fdaa31adc2aee03d0a6dc719d8094
Igor dos Reis <igorreis@gmail.com>
2022-07-18 14:54:36 -0300

    Exercicios do bloco 10.2.

---
+++ 2-front-end/b10-intro-react/d02-componentes-react/para_fixar/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Image from './Image';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+      </div> 
+    );
+  }
+}
+// function App() {
+//   return (
+//     <div>
+//       <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText="Cute cat staring" />
+//     </div>
+//   );
+// }
+export default App;
