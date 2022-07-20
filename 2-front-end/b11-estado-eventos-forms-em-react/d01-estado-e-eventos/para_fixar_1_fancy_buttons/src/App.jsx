3d022b20d05824257a174c0f89fb0e1aabceb759
Igor dos Reis <igorreis@gmail.com>
2022-07-20 20:24:33 -0300

    Exercicios para fixar do bloco 11.1.

--- 2-front-end/b11-estado-eventos-forms-em-react/d01-estado-e-eventos/para_fixar_1_fancy_buttons/src/App.jsx
+++ 2-front-end/b11-estado-eventos-forms-em-react/d01-estado-e-eventos/para_fixar_1_fancy_buttons/src/App.jsx
@@ -18,21 +18,44 @@ class App extends Component {
     console.log(Object.keys(this));
     console.log(Object.values(this));
     console.log('Texto 1');
-    this.setState((previousState, _props) => ({
-      numberOfClicksButtonOne: previousState.numberOfClicksButtonOne + 1
+    this.setState(( { numberOfClicksButtonOne }, _props) => ({ // Aqui foi usado desestruturacao na passagem do parametro e o retorno é implícito
+      numberOfClicksButtonOne: numberOfClicksButtonOne + 1 
     }))
   }
+
   clickCounter() {
-    this.setState((previousState, _props) => ({ // Props é o 2o parametro da funcao setState e ele é obrigatório, mas como ele não sera utilizado, a notação _props é usada para indicar isso
-      numberOfClicksButtonTwo: previousState.numberOfClicksButtonTwo + 1
-    })) // documentacao do setState https://pt-br.reactjs.org/docs/react-component.html#setstate
+    this.setState((previousState, _props) => { // Props é o 2o parametro da funcao setState e ele é obrigatório, mas como ele não sera utilizado, a notação _props é usada para indicar isso
+      return {numberOfClicksButtonTwo: previousState.numberOfClicksButtonTwo + 1}; // Aqui o retorno é explícito e por isso deve estar entre {}
+    }) // documentacao do setState https://pt-br.reactjs.org/docs/react-component.html#setstate
+  }
+    // clickCounter() {
+  //   this.setState((previousState, _props) => ({ // Props é o 2o parametro da funcao setState e ele é obrigatório, mas como ele não sera utilizado, a notação _props é usada para indicar isso
+  //     numberOfClicksButtonTwo: previousState.numberOfClicksButtonTwo + 1
+  //   })) // documentacao do setState https://pt-br.reactjs.org/docs/react-component.html#setstate
+
+  changeButtonColorWithStyle(number) {
+    return number % 2 === 0 ? 'green' : 'transparent';
+  }
+
+  changeButtonColorWithClass(number) {
+    return number % 2 === 0 ? 'buttonColor' : 'buttonBlank';
   }
 
   render() {
+    const { numberOfClicksButtonOne, numberOfClicksButtonTwo } = this.state;
     return (
       <div>
-        <button onClick={this.printText1}>Botao 1</button>
-        <button onClick={this.clickCounter}>{ this.state.numberOfClicksButtonTwo }</button>
+        <button 
+          style={{ backgroundColor: `${this.changeButtonColorWithStyle(numberOfClicksButtonOne)}`}} 
+          onClick={ this.printText1 }> 
+            Botao 1 
+        </button>
+
+        <button 
+          className={this.changeButtonColorWithClass(numberOfClicksButtonTwo)} 
+          onClick={ this.clickCounter }>
+            { `Clicks: ${numberOfClicksButtonTwo}` }
+        </button>
         {/* Se ao clicar no botao eu quisesse chamar uma funcao com parametro, posso usar a sintaxe abaixo */}
         {/* <button onClick={() => this.functionWithParam(param)}>{ this.state.numberOfClicks }</button>  */}
       </div>
