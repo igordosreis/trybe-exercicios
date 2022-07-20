bfe278b705cf58ce3f83751021606c775a948dff
Igor dos Reis <igorreis@gmail.com>
2022-07-20 19:06:00 -0300

    Exercicios do bloco 11.1.

--- 2-front-end/b11-estado-eventos-forms-em-react/d01-estado-e-eventos/para_fixar_1_fancy_buttons/src/App.jsx
+++ 2-front-end/b11-estado-eventos-forms-em-react/d01-estado-e-eventos/para_fixar_1_fancy_buttons/src/App.jsx
@@ -1,23 +1,40 @@
 import { Component } from 'react';
 import './App.css';
 
-function printText1() {
-  console.log('Texto 1')
-}
-function printText2() {
-  console.log('Texto 2')
-}
-function printText3() {
-  console.log('Texto 3')
-}
-
 class App extends Component {
+  // constructor(props) { // Caso seja necessario acessar uma props dentro do construtor
+  constructor() {
+    super()
+    this.state = {
+      numberOfClicksButtonOne: 0,
+      numberOfClicksButtonTwo: 0,
+    }
+    this.printText1 = this.printText1.bind(this);
+    this.clickCounter = this.clickCounter.bind(this);
+  }
+
+  printText1() { // Pode-se usar arrow functions para declarar a funcao dentro da clase, mas isso torna a aplicacao menos performatica
+    console.log(this);
+    console.log(Object.keys(this));
+    console.log(Object.values(this));
+    console.log('Texto 1');
+    this.setState((previousState, _props) => ({
+      numberOfClicksButtonOne: previousState.numberOfClicksButtonOne + 1
+    }))
+  }
+  clickCounter() {
+    this.setState((previousState, _props) => ({ // Props é o 2o parametro da funcao setState e ele é obrigatório, mas como ele não sera utilizado, a notação _props é usada para indicar isso
+      numberOfClicksButtonTwo: previousState.numberOfClicksButtonTwo + 1
+    })) // documentacao do setState https://pt-br.reactjs.org/docs/react-component.html#setstate
+  }
+
   render() {
     return (
       <div>
-        <button onClick={printText1}>Botao 1</button>
-        <button onClick={printText2}>Botao 2</button>
-        <button onClick={printText3}>Botao 3</button>
+        <button onClick={this.printText1}>Botao 1</button>
+        <button onClick={this.clickCounter}>{ this.state.numberOfClicksButtonTwo }</button>
+        {/* Se ao clicar no botao eu quisesse chamar uma funcao com parametro, posso usar a sintaxe abaixo */}
+        {/* <button onClick={() => this.functionWithParam(param)}>{ this.state.numberOfClicks }</button>  */}
       </div>
     );
   }
